34c608565dafbe11fa844e93d42f8d185b0dc45f
Sebastian Gassner <sebastian.gassner@gmail.com>
2022-02-09 14:24:36 +0100

    adding vanilla samples (not using AUT)

--- lib/warc2corpus/__init__.py
+++ lib/warc2corpus/__init__.py
@@ -1,2 +1,43 @@
-import warc2corpus
-import warc2corpus.extractors
+import bs4
+from datetime import datetime as dt
+
+def apply(content,extractors):
+    """
+    Apply extractors to content.
+    """
+    if not isinstance(extractors,(list,tuple)):
+        extractors = [extractors]
+    soup = bs4.BeautifulSoup(content,'lxml')
+    result = []
+    for extractor in extractors:
+        meta = extractor.get('meta',{})
+        meta['created_at'] = dt.now().isoformat()
+        data = {}
+        for e in extractor['extracts']:
+            n = e.get('name','n/a')
+            c = e.get('css_path',None)
+            f = e.get('f',None)
+            if not c:
+                continue
+            text = soup.select(c)
+            if text and f:
+                text = f(text)
+            elif text:
+                # Join all text rows,
+                # if no lambda has been given,
+                text = ' '.join([ m.get_text().strip() for m in text ])
+            data[n] = {
+                'css_path': c,
+                'value': (text or None)
+            }
+            # Check, if extracted data is valid, store under 'meta.valid';
+            # the extractor may specify a callable under 'validator';
+            # if 'validator' exists, insert its return value under 'meta.valid';
+            # if it does not exist, check, if all extracted values are non-empty.
+            if not 'validator' in extractor:
+                # (not not on an empty string returns False.)
+                extractor['validator'] = lambda pairs: all([ not not v for k,v in pairs ])
+                pairs = list(((k,v['value']) for k,v in data.items()))
+                meta['valid'] = extractor['validator'](pairs)
+        result.append({'meta':meta,'data':data})
+    return result
---
+++ sample/sample.py
@@ -0,0 +1,50 @@
+# make console
+# /spark/bin/spark-submit --py-files /aut/target/aut.zip --jars=/aut/target/aut-0.91.1-SNAPSHOT-fatjar.jar sample/sample.py
+
+from pathlib import Path
+import re
+import json
+import dateparser as dp
+import warc2corpus as w2c
+
+base= Path(__file__).parent.parent
+datadir= base.joinpath('data')
+samplefile= datadir.joinpath('sueddeutsche_artikel_20220208.html')
+html= open(samplefile,'r',encoding='utf-8').read()
+
+extractor= {
+    'meta': {
+        'name': 'Sueddeutsche Artikel',
+        'issuer': 'media_site',
+        'platform': 'website',
+        'type': 'article',
+        'layout': 'a'
+    },
+    'netloc_regex': re.compile('www\.zeit\.de'),
+    'path_regex': re.compile('^/news/.+$'),
+    'extracts': [
+        {
+            'name': 'title',
+            'css_path': 'article header h2',
+            'f': lambda m: m[0].get_text().strip().split('|')[0]
+        },
+        {
+            'name': 'body',
+            'css_path': 'div[itemprop~="articleBody"] p',
+        },
+        {
+            'name': 'author',
+            'css_path': 'article > p:first-of-type',
+            'f': lambda m: re.sub('^Von ', '', m[0].get_text())
+        },
+        {
+            'name': 'released_at',
+            'css_path': 'header time',
+            'f': lambda m: dp.parse(m[0]['datetime'].strip(), date_formats=['%d. %B %Y']).isoformat()
+        }
+    ]
+}
+
+result= w2c.apply(html, extractor)
+
+print(json.dumps(result,indent=4,sort_keys=True))
